1fb66f10c32080272d58df4986b62cf47bd3376f
Жавохир Хакберидиев <haqberdiyev99@bk.ru>
2023-01-20 18:43:08 +0500

    done!

--- task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/impl/RectangleObserver.java
@@ -1,5 +1,26 @@
 package com.mjc.stage2.impl;
 
-public class RectangleObserver  {
-    // Write your code here!
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.entity.Rectangle;
+import com.mjc.stage2.entity.RectangleValues;
+import com.mjc.stage2.event.RectangleEvent;
+import com.mjc.stage2.warehouse.RectangleWarehouse;
+
+public class RectangleObserver  implements Observer {
+
+    @Override
+    public void handleEvent(RectangleEvent event) {
+        Rectangle rectangle = event.getSource();
+        double sideA = rectangle.getSideA();
+        double sideB = rectangle.getSideB();
+        double square = sideA * sideB;
+        double perimeter = 2 * (sideA + sideB);
+
+        RectangleValues rectangleValues = RectangleWarehouse.getInstance().get(rectangle.getId());
+        if (rectangleValues != null){
+            rectangleValues.setPerimeter(perimeter);
+            rectangleValues.setSquare(square);
+        }
+        RectangleWarehouse.getInstance().put(rectangle.getId(), rectangleValues);
+    }
 }
--- task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
+++ task3-handling/src/main/java/com/mjc/stage2/entity/SymbolLeaf.java
@@ -1,8 +1,34 @@
 package com.mjc.stage2.entity;
 
-public class SymbolLeaf {
+import javax.print.DocFlavor;
+
+public class SymbolLeaf extends AbstractTextComponent{
 
     private char value;
 
-    // Write your code here!
+    public SymbolLeaf(TextComponentType componentType, char value) {
+        super(componentType);
+        this.value = value;
+    }
+
+    @Override
+    public String operation() {
+        return String.valueOf(value);
+    }
+
+    @Override
+    public void add(AbstractTextComponent textComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(AbstractTextComponent textComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getSize() {
+        throw new UnsupportedOperationException();
+    }
+
 }
